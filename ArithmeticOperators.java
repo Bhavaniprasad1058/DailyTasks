798663f762baca9280fa77dc7b7f1393b94ab115
Bhavaniprasad1058 <137038224+Bhavaniprasad1058@users.noreply.github.com>
2024-07-12 13:22:37 +0530

    Add files via upload

---
+++ ArithmeticOperators.java
@@ -0,0 +1,15 @@
+package Tasks;
+
+public class ArithmeticOperators {
+
+	public static void main(String[] args) {
+       int a=10;
+       int b=20;
+       System.out.println(a+b);
+       System.out.println(a*b);
+       System.out.println(a/b);
+       System.out.println(a%b);
+       
+	}
+
+}
