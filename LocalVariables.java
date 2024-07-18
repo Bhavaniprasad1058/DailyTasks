07c4c3e846c02ce406cc8dd03262e362310b6eac
Bhavaniprasad1058 <137038224+Bhavaniprasad1058@users.noreply.github.com>
2024-07-18 09:54:58 +0530

    Add files via upload

---
+++ LocalVariables.java
@@ -0,0 +1,12 @@
+package Tasks;
+
+public class LocalVariables {
+
+	public static void main(String[] args) {
+      int i=10;
+      String name="Bhavani Prasad";
+      System.out.println(name);
+      System.out.println(i);
+	}
+
+}
--- LogicalOperators.java
+++ LogicalOperators.java
@@ -5,10 +5,9 @@ public class LogicalOperators {
 	public static void main(String[] args) {
       int a=10;
       int b=20;
-      System.out.println(a+=b);
-      System.out.println(a*=b);
-      System.out.println(a/=b);
-      System.out.println(a%=b);
+      System.out.println(a>b&&a<b);
+      System.out.println(a>b||a<b);
+      
       
 	}
 
