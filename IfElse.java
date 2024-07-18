03fe46285fc552abc425ce71141daf46fef2a9bb
Bhavaniprasad1058 <137038224+Bhavaniprasad1058@users.noreply.github.com>
2024-07-18 10:19:04 +0530

    Add files via upload

---
+++ IfElse.java
@@ -0,0 +1,14 @@
+package Tasks;
+
+public class IfElse {
+
+	public static void main(String[] args) {
+      int a=10;
+      if (a>20) {
+		System.out.println("a is greater than 20");
+	} else {
+        System.out.println("a is lesser than 20");
+	}
+	}
+
+}
---
+++ IfStatement.java
@@ -0,0 +1,12 @@
+package Tasks;
+
+public class IfStatement {
+
+	public static void main(String[] args) {
+          int a=20;
+          if(a>10) {
+        	  System.out.println("a is greater than 10");
+          }
+	}
+
+}
