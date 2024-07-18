42cdc7b97767e8ac2d6489946a27acceb19b87dd
Bhavaniprasad1058 <137038224+Bhavaniprasad1058@users.noreply.github.com>
2024-07-18 10:06:40 +0530

    Add files via upload

---
+++ StaticVariables.java
@@ -0,0 +1,9 @@
+package Tasks;
+
+public class StaticVariables {
+	  //declare static variables
+	   public static String name="Bhavani Prasad";
+         public static void main(String[] args) {
+			System.out.println(StaticVariables.name);
+		}
+}
